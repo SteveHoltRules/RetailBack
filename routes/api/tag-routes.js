248d75c1e2cfcc2e6347504ac9c5fae143669ebd
Matt Luna <mat.c.luna@gmail.com>
2021-11-07 16:36:50 -0600

    fixed tag update endpoint

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -56,16 +56,16 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update(
-    {
-      tag_name: req.body.tag_name,
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
     },
-    {
-      where: { 
-        id: req.params.id 
-      },
-    }
-  );
+  })
+    .then((dbTagData) => res.status(200).json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });;
 });
 
 router.delete("/:id", (req, res) => {
@@ -75,17 +75,17 @@ router.delete("/:id", (req, res) => {
       id: req.params.id,
     },
   })
-  .then((dbTagData) => {
-    if(!dbTagData) {
-      res.status(404).json({ message: "No tag found with this id"});
-      return;
-    }
-    res.json(dbTagData);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
